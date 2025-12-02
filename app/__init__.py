9a5a7239d4c4694a4805f1694757004cc2ad03ea
killersin122107 <121577371+killersin122107@users.noreply.github.com>
2025-12-02 11:33:13 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,1 @@
-/ (Project Root)
-├── bot.py
-├── requirements.txt
-├── app/
-│   ├── __init__.py  <-- 💡 This is the crucial file you need to add!
-│   ├── database.py
-│   ├── models.py
-│   └── checker.py
-└── ...
+
